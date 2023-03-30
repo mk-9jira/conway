486b91bb5105ebc678fdc9c9629b6afbc0a6f2f5
mk-9jira <koji_matsukura@caddi.jp>
2023-03-31 00:11:05 +0900

    implement the rules: evolve generations on space key

--- src/main.nim
+++ src/main.nim
@@ -14,38 +14,65 @@ let
   nx = wx div dx
   ny = wy div dy
 
-var board = newSeqWith(nx, newSeq[bool](ny))
-board[0][0] = true
-board[10][0] = true
-board[0][5] = true
-
-# var buttonDown = false
+var boardCurrent = newSeqWith(nx, newSeq[bool](ny))
+var boardNext = newSeqWith(nx, newSeq[bool](ny))
+boardCurrent[0][0] = true
+boardCurrent[nx-1][ny-1] = true
+boardCurrent[10][10] = true
 
 proc gameInit() =
   loadFont(0, "font.png")
+
+# count alive/dead cells around the specified cell
+proc neighbourCount(i, j: int): int =
+  for di in -1..1:
+    for dj in -1..1:
+      let ii = (i + di + nx) mod nx   # 0 <= ii < nx
+      let jj = (j + dj + ny) mod ny   # 0 <= jj < ny
+      result += int(boardCurrent[ii][jj])
+  result -= int(boardCurrent[i][j])
+
+proc updateBoard() =
+  for i in 0..<nx:
+    for j in 0..<ny:
+      let isAlive = boardCurrent[i][j]
+      let n = neighbourCount(i, j)
+      if not isAlive and n == 3:
+        boardNext[i][j] = true
+      elif isAlive and (n < 2 or n > 3):
+        boardNext[i][j] = false
+      else:
+        boardNext[i][j] = boardCurrent[i][j]
+#[ debug -->
+      if n > 0:
+        echo "i = ", i, " j = ", j, " n = ", n
+      elif n < 0:
+        echo "Error: n should not be negative"
+<-- debug ]#
+  swap(boardCurrent, boardNext)
 
 proc toggleCellInCurrent(x, y: int) =
     let i = x div dx
     let j = y div dy
     if i < 0 or i >= nx or j < 0 or j >= ny:
       return
-    board[i][j] = not board[i][j]
+    boardCurrent[i][j] = not boardCurrent[i][j]
 
 proc gameUpdate(dt: float32) =
-  # buttonDown = btn(pcA)
+  if key(K_SPACE):
+    updateBoard()
+    sleep(200)
   if mousebtn(0):
     let (x, y) = mouse()
     toggleCellInCurrent(x, y)
-    # echo "(x, y) = (", x, ", ", y, ")"
     sleep(100)
 
 proc gameDraw() =
   cls()
-  # setColor(if buttonDown: 7 else: 3)
   # printc("welcome to " & appName, screenWidth div 2, screenHeight div 2)
   for i in 0..<nx:
     for j in 0..<ny:
-      if board[i][j] == true:
+      if boardCurrent[i][j] == true:
         setColor(3)
         boxfill(i*dx, j*dy, dx, dy)
       else:
